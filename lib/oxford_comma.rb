b9888a07d099abbc1c69cd04902ca49c03d1533a
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 00:24:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 array = ["1", "2", "3", "4"]
 def oxford_comma(array)
   if array.length >= 3 
-    string = array.(length-1)
+    
+    string = array[-2]
     string.join(" , ")
     puts string 
   else
