12ef1c216e75c9bc63e6fca1561d2936d32150c0
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 00:45:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,7 @@
 def oxford_comma(array)
   if array.length == 2 
     return array.join(" and ")
-  elsif array.length == 3 
-    last_element = array.pop
-    return array.join (" , ") << ", and " + last_element
-  elseif array.length >= 3
+  elsif array.length >= 3
     last_element = array.pop
     array.length[-2].insert(0, ", ")
     return array.join (" , ") <<  " and " + last_element
