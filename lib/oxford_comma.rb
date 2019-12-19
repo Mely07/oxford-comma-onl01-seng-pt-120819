70156b236498c0ad11cbecfdfbd6619354bc949d
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 00:20:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+array = [hi hola chao bye adios]
 def oxford_comma(array)
-
+  if array.length >= 3 
+    string = array.join(" , ")
+  else
+   return array.join(" & ")
 end
+
+
+oxford_comma(array)
