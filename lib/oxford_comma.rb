74d13eddda753b5fe84223858416a4c7cd70f95e
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 00:23:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
-array = [hi hola chao bye adios]
+array = ["1", "2", "3", "4"]
 def oxford_comma(array)
   if array.length >= 3 
-    string = array.join(" , ")
+    string = array.(length-1)
+    string.join(" , ")
+    puts string 
   else
-   return array.join(" & ")
+   return array
 end
-
 
 oxford_comma(array)
