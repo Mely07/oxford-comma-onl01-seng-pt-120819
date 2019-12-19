fac4574d4a18a85dab8a61e92d449d5e3b148473
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 00:32:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length == 3 
     last_element = array.pop
-    return array.join (" , ") << "and," + last_element
+    return array.join (" , ") << ", and " + last_element
   else
    return array.join (" , ")
   end
